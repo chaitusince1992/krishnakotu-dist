91086ec1409cee55366561725c8b40fdbe150b0a
Krishna Chaitanya  Kotu <krishna.kotu@imaginea.com>
2018-01-18 23:54:18 +0530

    version 4

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ var express = require('express');
 var fs = require('fs');
 var path = require('path');
 var app = express();
-app.use(express.static(path.join(__dirname, '/dist')));
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.use(express.static(__dirname + '/dist'));
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html');
 });
+
+//app.use('/dist', routesApi);
+/*app.use(function(req, res) {
+    res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+app.get('*', function(req, res) {
+  res.sendfile(__dirname + '/index.html')
+})*/
+
+
 var server = app.listen(process.env.PORT || 5000, function () {
     console.log("Listening on port: ", server.address().port);
 })
